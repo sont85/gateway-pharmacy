97cc7bebe8591a4d3952dc21ddfcec450be1549a
Son Truong <sont85@gmail.com>
2015-11-06 14:13:33 -0800

    setup environment

---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'public/css/style.css': 'sass/style.scss'
+        }
+      }
+    },
+    uglify: {
+      dist: {
+        files: {
+          'public/javascripts/main.min.js': 'javascripts/main.js'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['sass/style.scss', 'sass/_base.scss'],
+        tasks: ['sass']
+      },
+      javascripts: {
+        files: ['javascripts/main.js'],
+        tasks: ['uglify']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['sass', 'uglify']);
+}
